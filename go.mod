28d728ce51329a0b8573ca079cb509168de51374
liujintao <liujt@ncmps.com>
2025-05-30 13:42:42 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/simon1000-wow/ljt_tool
+
+go 1.23.4
---
+++ string/reverse.go
@@ -0,0 +1,11 @@
+// stringutil/reverse.go
+package stringutil
+
+// 导出函数首字母大写
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
